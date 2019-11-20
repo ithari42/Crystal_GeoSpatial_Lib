0413f28ba7675cee98026b692b41b9701445e6c0
CGS_DEV <jpritchett42@gmail.com>
2019-11-19 19:59:38 -0500

    some stuff

--- CGS_Test_App/src/geo.cr
+++ CGS_Test_App/src/geo.cr
@@ -1,17 +1,26 @@
 class GeoPoint
-  def initialize(@lat : Float64, @lon : Float64, @alt : Float64)
+  def initialize(lat : Float64, lon : Float64, alt : Float64)
+		@lat=lat
+		@lon=lon
+		@alt=alt
   end
 
   def coordinate2d
-    {@lat,@lon}
+		
+		{@lat,@lon}
+    
   end
 
   def coordinate3d
     {@lat,@lon,@alt}
   end
 end
+
 class GeoPolyline
-	def initialize(@points)#Need a tuple of GeoPoint
+	def initialize(points)#Need a tuple of GeoPoint
+		@array_of_geo =[] of GeoPoint
+		@array_of_geo= points
+		
 	end
 
 	def toPolygon #return a polygon formed by connecting head and rear Geopoint
@@ -19,9 +28,37 @@ class GeoPolyline
 	end
   
 	def allNorth # returns true if all of the points are in the northeren hemisphere, false otherwise
+				x=0
+				flag=true
+				while x < @array_of_geo.size && flag ==true
+				if @array_of_geo[x].@lat < 0 || @array_of_geo[x].@lon < 0 || @array_of_geo[x].@alt <0        
+					puts "some of your your points are south "
+					flag=false
+				end
+	            x=x+1
+				end
+	if flag == true
+	puts "your points are north"
+	end
+
+	
+      		
 	end
 	
 	def allSouth # returns true if all of the points are in the southern hemisphere, false otherwise
+				x=0
+				flag=true
+				while x < @array_of_geo.size && flag==true
+				if @array_of_geo[x].@lat > 0 || @array_of_geo[x].@lon > 0 || @array_of_geo[x].@alt >0        
+					puts "some of your points are north "
+					flag=false
+				end
+	            x=x+1
+				end
+	if flag == true
+	puts "your points are south"
+	end
+
 	end
 	
 end
@@ -72,3 +109,14 @@ class GeoUtilities2D
     
   end
 end
+
+k=GeoPoint.new(3,3,3)
+v=GeoPoint.new(1,2,4)
+array_of_geo = [] of GeoPoint
+array_of_geo << k
+array_of_geo << v
+z=GeoPolyline.new(array_of_geo)
+z.allNorth
+z.allSouth
+# z.array_of_geo[1].@lat
+
