1654172a9ff1778ead56703ae0709f9f5b382533
nobsessive <33778469+nobsessive@users.noreply.github.com>
2019-11-23 10:57:57 -0500

    fixed some bugs

--- CGS_Test_App/src/CGS_Test_App.cr
+++ CGS_Test_App/src/CGS_Test_App.cr
@@ -6,12 +6,15 @@ module CGSTestApp
   VERSION = "0.1.0"
 
   # Our algorithms are written in ../src/geo
-  util=GeoUtilities2D.new
+  #util=GeoUtilities2D.new
   p1=GeoPoint.new(38.9, 77.0, 1.0) # 73.1 N, 77.0 S, 1.0m 
+  p2=GeoPoint.new(38.9, 78.0, 1.0)
+  l1=GeoPolyline.new([p1,p2])
+
 
   # run tester to see if intersections formed by our shapes can be judged properly by our functions in geo
   # we need to tell tester the right answer to tester firstly 
-  test=Tester.new
-  test.autoTest
+  #test=Tester.new
+  #test.autoTest
 
 end
--- CGS_Test_App/src/geo.cr
+++ CGS_Test_App/src/geo.cr
@@ -5,14 +5,20 @@ class GeoPoint
 		@alt=alt
   end
 
+  def lat
+	@lat
+  end
+
+  def lon
+	@lon
+  end
+
   def coordinate2d
-		
-		{@lat,@lon}
-    
+		[@lat,@lon]
   end
 
   def coordinate3d
-    {@lat,@lon,@alt}
+    [@lat,@lon,@alt]
   end
 end
 
@@ -23,42 +29,38 @@ class GeoPolyline
 		
 	end
 
+	def data
+		@array_of_geo
+	end
+
 	def toPolygon #return a polygon formed by connecting head and rear Geopoint
 		return GeoPolygon.new(@points)
 	end
   
 	def allNorth # returns true if all of the points are in the northeren hemisphere, false otherwise
-				x=0
-				flag=true
-				while x < @array_of_geo.size && flag ==true
-				if @array_of_geo[x].@lat < 0 || @array_of_geo[x].@lon < 0 || @array_of_geo[x].@alt <0        
-					puts "some of your your points are south "
+		x=0
+		flag=true
+		while x < @array_of_geo.size && flag ==true
+			if @array_of_geo[x].@lat < 0        
+					#puts "some of your your points are south "
 					flag=false
-				end
-	            x=x+1
-				end
-	if flag == true
-	puts "your points are north"
-	end
-
-	
-      		
+			end
+	        x=x+1
+		end	
+		return x	
 	end
 	
 	def allSouth # returns true if all of the points are in the southern hemisphere, false otherwise
-				x=0
-				flag=true
-				while x < @array_of_geo.size && flag==true
-				if @array_of_geo[x].@lat > 0 || @array_of_geo[x].@lon > 0 || @array_of_geo[x].@alt >0        
-					puts "some of your points are north "
-					flag=false
-				end
-	            x=x+1
-				end
-	if flag == true
-	puts "your points are south"
-	end
-
+		x=0
+		flag=true
+		while x < @array_of_geo.size && flag==true
+			if @array_of_geo[x].@lat         
+				puts "some of your points are north "
+				flag=false
+			end
+	        x=x+1
+		end
+		return x
 	end
 	
 end
@@ -94,7 +96,15 @@ class GeoUtilities2D
   end
 
   def intersects(p : GeoPoint, l : GeoPolyline)
-    
+	flag=false
+	line=l.data
+	x=0
+	n=line.size
+	while x<n-1 
+		subline=get_subline(l,x,x+1)
+		#if on_subline(p,subline) ...
+		x+=1
+	end
   end
 
   def intersects(p : GeoPoint, g : GeoPolygon)
@@ -108,15 +118,36 @@ class GeoUtilities2D
   def intersects(g1 : GeoPolygon, g2 : GeoPolygon)
     
   end
+#utile functions
+
+#Input: subline must be [[float64, float64],[float64, float64]]
+  def v_dotProduct(v1,v2)
+	if v1.size>2||v2.size>2
+		"dot product error"
+	end
+	return (v1[0]*v2[1]-v1[2]*v2[1])
+  end
+#return [[lat1,lon1],[lat2,lon2]]
+#ith subline (i,i+1), i starts with 0
+#i must be i<size-1
+  def get_subline(l : GeoPolyline, i : Int32)
+	if i>=l.size||j>=l.size
+		"get_subline out of range"
+	end
+	d=l.data
+	return [[d[i].lat,d[i].lon],[d[i+1].lat,d[i+1].lon]]
+  end
+
+  def on_subline(p : Geopoint, s)
+	
+  end
+
+  def enum_polygon(l : GeoPolyline) # return a tuple containing each vector on a polygon
+	
+  end
 end
 
-k=GeoPoint.new(3,3,3)
-v=GeoPoint.new(1,2,4)
-array_of_geo = [] of GeoPoint
-array_of_geo << k
-array_of_geo << v
-z=GeoPolyline.new(array_of_geo)
-z.allNorth
-z.allSouth
-# z.array_of_geo[1].@lat
-
+"geo.cr test"
+p1=GeoPoint.new(38.9, 77.0, 1.0) # 73.1 N, 77.0 S, 1.0m 
+p2=GeoPoint.new(38.9, 78.0, 1.0)
+l1=GeoPolyline.new([p1,p2])
