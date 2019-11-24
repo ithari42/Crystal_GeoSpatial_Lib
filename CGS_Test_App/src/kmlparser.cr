e4917f1cb68d402e0f9ee0f68dc2c31d71eedcfb
CGS_DEV <jpritchett42@gmail.com>
2019-11-24 16:54:54 -0500

    Implemented kml parser

---
+++ CGS_Test_App/src/kmlparser.cr
@@ -0,0 +1,122 @@
+require "xml"
+require "file"
+require "io"
+require "../src/geo"
+
+def parseKML(filename : String)
+
+	xmlstring = File.read(filename)
+	xmldoc = XML.parse(xmlstring)
+	
+	root = xmldoc.first_element_child
+  shape_name = ""
+  isPolygon = false
+  isLine = false
+  isPoint = false
+  coordinate_string = ""
+  lon_string = ""
+  lat_string = ""
+  alt_string = ""
+	
+	if root
+		root.children.select(&.element?).each do |child|
+			if child.name == "Document"
+        document = child
+        document.children.select(&.element?).each do |child|
+          if child.name == "Placemark"
+            placemark = child
+            placemark.children.select(&.element?).each do |child|
+              if child.name == "name"
+                shape_name = child.content
+              end
+              if child.name == "Polygon"
+                polygon = child
+                isPolygon = true
+                polygon.children.select(&.element?).each do |child|
+                  if child.name == "outerBoundaryIs"
+                    outer = child
+                    outer.children.select(&.element?).each do |child|
+                      if child.name == "LinearRing"
+                        ring = child
+                        ring.children.select(&.element?).each do |child|
+                          if child.name == "coordinates"
+                            coordinate_string = child.content
+                          end 
+                        end
+                      end          
+                    end
+                  end
+                end
+              end 
+              if child.name == "LineString" 
+                line = child
+                isLine = true
+                line.children.select(&.element?).each do |child|
+                  if child.name == "coordinates"
+                    coordinate_string = child.content
+                  end
+                end
+              end
+              if child.name == "LookAt"
+                point = child
+                isPoint = true
+                point.children.select(&.element?).each do |child|
+                  if child.name == "longitude"
+                    lon_string = child.content
+                  end
+                  if child.name == "latitude"
+                    lat_string = child.content
+                  end
+                  if child.name == "altitude"
+                    alt_string = child.content
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+		end
+      
+	end
+  
+  if isPolygon
+    arr1 = coordinate_string.split()
+    pointList = [] of GeoPoint
+    arr1.each do |string|
+      arr = string.split(",", remove_empty: true)
+      point = GeoPoint.new(arr[0].to_f,arr[1].to_f,arr[2].to_f)
+      pointList << point
+    end
+    polygon = GeoPolygon.new(pointList)
+    return {"polygon", shape_name, polygon,}
+    
+  elsif isLine
+    arr1 = coordinate_string.split()
+    pointList = [] of GeoPoint
+    arr1.each do |string|
+      arr = string.split(",", remove_empty: true)
+      point = GeoPoint.new(arr[0].to_f,arr[1].to_f,arr[2].to_f)
+      pointList << point
+    end
+    polyline = GeoPolyline.new(pointList)
+    return {"polyline", shape_name, polyline}
+    
+  elsif isPoint
+    point = GeoPoint.new(lon_string.to_f,lat_string.to_f,alt_string.to_f)
+    return {"point", shape_name, point}
+    
+  else
+    return {"nil", shape_name, nil}
+  end
+ 
+end
+
+
+
+
+
+
+
+
+
